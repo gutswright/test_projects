438fcdcf5dad446d0d135ddbdc9e1e02f4323256
Guts Wright <gutswright@maul.local>
2025-03-31 11:10:53 -0700

    Added up through most of part 2 of the online tutorial

---
+++ swift/swift_test_app/swift_test_app/mainApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_test_appApp.swift
+//  swift_test_app
+//
+//  Created by Guts Wright on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct mainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
